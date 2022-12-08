9928c82c7510183458bfaed2d4a495ed10755670
Aasiyaali1 <113003343+Aasiyaali@users.noreply.github.com>
2022-12-08 16:39:47 +0000

    set 4 problem 8

--- set02/problem05.c
+++ set02/problem05.c
@@ -1,1 +1,36 @@
 #include<stdio.h>
+#include<math.h>
+
+long fact(int n){
+    if(n==0)
+        return 1;
+    return n*fact(n-1);
+}
+
+int input(int x){
+    scanf("%d",&x);
+    return x;
+}
+float borga_X(int x){
+    float res=0.0;
+    double next = 1.0;
+    int i=2;
+    while(next >= 0.000001){
+        res+=next;
+        next=pow(x,i-1)/(fact(2*i-1)*1.0);
+        i++;
+    }
+    return res;
+}
+void output(int x, float result){
+    printf("borga(%d) = %f",x,result);
+}
+
+int main(){
+    int x;
+    x=input(x);
+    float result = borga_X(x);
+    output(x,result);
+    return 0;
+}
+
